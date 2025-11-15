2bedf7a629a1bc72934aacda7430786930903e99
prakashleena <kiboxsonleena2004@gmail.com>
2025-11-15 15:28:04 +0530

    update

--- frontend/src/pages/Members.js
+++ frontend/src/pages/Members.js
@@ -57,7 +57,7 @@ const MemberCard = memo(({ member }) => {
             <p className="text-base text-gray-600">{member.universityOrRole}</p>
           )}
           {/* Debug: Show all member data */}
-          <pre className="text-xs text-gray-400 mt-2 hidden">
+          <pre className="text-xs text-gray-400 mt-2 block">
             {JSON.stringify(member, null, 2)}
           </pre>
         </div>
